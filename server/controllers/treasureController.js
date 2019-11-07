0054816fa9f2d6ffc0b21156b91839ddfb752c51
WillHoll <willhollingsworth15@yahoo.com>
2019-11-06 19:02:44 -0700

    complete

--- server/controllers/treasureController.js
+++ server/controllers/treasureController.js
@@ -7,5 +7,16 @@ module.exports = {
     getUserTreasure: async (req, res) => {
         const usertreasure = await req.app.get('db').get_user_treasure([req.session.user.id])
         res.status(200).send(usertreasure)
+    },
+    addUserTreasure: async (req, res) => {
+        const {treasureURL} = req.body;
+        const {id} = req.session.user;
+        const userTreasure = await req.app.get('db').add_user_treasure([treasureURL, id]);
+        res.status(200).send(userTreasure);
+    },
+    getAllTreasure: async (req, res) => {
+        const db = req.app.get('db')
+        const allTreasure = await db.get_all_treasure
+        res.status(200).send(allTreasure)
     }
 }
